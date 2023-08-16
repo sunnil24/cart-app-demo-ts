0cc005788dcd291fea8d5da9221d43d9bf960c5f
sunnil24 <sunilsri.srivastava3@gmail.com>
2023-08-16 18:11:19 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -99,14 +99,14 @@ class Product {
     target.classList.remove('no-result-found');
     const output = this.searchlist?.map(({ title, price }) => {
       const li = document.createElement('li')! as HTMLLIElement;
-      li.innerHTML = `<p>${title} <span class="green">[@ $${price.toLocaleString()}]</span></p>`;
+      li.innerHTML = `<p>${title} <span class="green">@ $${price.toLocaleString()}</span></p>`;
       target.insertAdjacentElement('beforeend', li);
       return title;
     });
 
     if (!output.length) {
       const li = document.createElement('li')! as HTMLLIElement;
-      li.innerHTML = 'No result found, try with something else!!';
+      li.innerHTML = 'Opps! No result found, try with something else.';
       target.classList.add('no-result-found');
       target.insertAdjacentElement('beforeend', li);
     }
